60c424750ee4f03b8a0befe36a68e32436cddcf8
arosen <adon.rosen@gmail.com>
2020-02-13 14:50:28 -0600

    Added the FA decomp - and unmixing -- need to verify

--- simulateData/runFA/runFAFullTS.R
+++ simulateData/runFA/runFAFullTS.R
@@ -4,7 +4,7 @@
 ## and then run a EFA on these data
 
 ## Load library(s)
-install_load("R.matlab", "tidyverse","reshape2", "psych")
+install_load("R.matlab", "tidyverse","reshape2", "psych", "greta","gganimate", "plotly")
 
 ## Declare statics and set wd
 setwd("~/Documents/dFAonEEG/simulateData/runFA/")
@@ -41,7 +41,7 @@ plot.data <- reshape2::melt(data = plot.data, direction = 'wide', idvar = 'color
 out.plot.2 <- ggplot(data=plot.data, aes(x=value, y=color, group=color, color=factor(color))) +
   geom_point() +
   theme_bw() +
-  theme(legend.position = "none") +
+  theme(legend.position = "none",text = element_text(size=20)) +
   ylab("Epoch") +
   xlab("Time (ms)")
 
@@ -50,15 +50,14 @@ out.plot.2.1 <- out.plot.2 <- ggplot(data=plot.data[index,], aes(x=value, y=colo
   geom_point() +
   geom_line() +
   theme_bw() +
-  theme(legend.position = "none") +
+  theme(legend.position = "none",text = element_text(size=20)) +
   ylab("Epoch") +
   xlab("Time (ms)")
 
 ## Now create a corllation matrix for the first epoch
 subj <- paste("../calhounScripts/ss2_create_sim/", in.data[4], sep='')
 tmp.in <- readMat(subj)
-plot(tmp.in$outdata[1,,1], tmp.in$outdata[2,,1])
-pc.loadings.one <- principal(r = as.matrix(tmp.in$outdata[,,1]), nfactors = 20)
+out.fa <- fa()
 cor.vals <- cor(tmp.in$outdata[,,1])
 cor.vals <- melt(cor.vals)
 out.plot.3 <- ggplot(data = cor.vals, aes(x=Var1, y=Var2, fill=value)) + 
@@ -69,5 +68,132 @@ out.plot.3 <- ggplot(data = cor.vals, aes(x=Var1, y=Var2, fill=value)) +
                        name="Pearson\nCorrelation") +
   theme_minimal()+ # minimal theme
   theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1))+
+                                   size = 12, hjust = 1),legend.position='none')+
+  coord_fixed()
+
+## Now plot the correlation matrix across time in a animated fashion
+## In order to do this we are going to iterate through every epoch and create a corellation matrix
+## and then collapse it into one object
+dataForAnim <- NULL
+for(t in 1:dim(tmp.in$outdata)[3]){
+  cor.vals <- cor(tmp.in$outdata[,,t])
+  cor.vals <- melt(cor.vals)
+  cor.vals$epoch <- t
+  dataForAnim <- rbind(dataForAnim, cor.vals)
+}
+out.plot.4 <- ggplot(data=dataForAnim, aes(x=Var1, y=Var2, fill=value, frame=epoch)) +
+  geom_tile() +
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1),legend.position='none')+
+  coord_fixed()
+p <- ggplotly(out.plot.4)
+
+plot(tmp.in$outdata[1,,1], tmp.in$outdata[2,,200])
+pc.loadings.one <- principal(r = as.matrix(tmp.in$outdata[,,1]), nfactors = 4)
+cor.vals <- cor(tmp.in$outdata[,,1])
+cor.vals <- melt(cor.vals)
+out.plot.3 <- ggplot(data = cor.vals, aes(x=Var1, y=Var2, fill=value)) + 
+  geom_tile() +
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                       midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1),legend.position='none') +
+  coord_fixed()
+
+## Now perform FA on the spatiospectral data
+# first we are going to have to reformat the input data
+my_matrix<-data.frame()
+for(j in 1:dim(tmp.in$outdata)[3]){
+  my_matrix <- rbind(my_matrix, tmp.in$outdata[,,j])
+}
+## Now we can fun the FA on the three d data collapsed across time
+fa.res <- fa(my_matrix, nfactors = 4, rotate = "promax")
+
+## Now I need to look at factor scores across time
+score.mat <- matrix(NA, dim(my_matrix)[1], 4)
+dim(score.mat) <- c(60,4,379)
+for(q in 1:dim(score.mat)[3]){
+  score.vals <- predict(fa.res, tmp.in$outdata[,,q])
+  score.mat[,,q] <- score.vals
+}
+
+## Now try to produce the heat map from figure 2
+# I am going to "unmix" the sources based on the FA weights
+# in order to do this I am going to multiply the resultant scores by the individual weights from each components/factor
+vals.one <- score.mat[,1,1] %*% t(fa.res$weights[,1])
+tmp <- tmp.in$outdata
+for(q in 1:dim(score.mat)[3]){
+  tmp[,,q] <- score.mat[,1,q]%*% t(fa.res$weights[,1])
+}
+vals.one <- apply(tmp, c(1,2), mean)
+tmp <- tmp.in$outdata
+for(q in 1:dim(score.mat)[3]){
+  tmp[,,q] <- score.mat[,2,q]%*% t(fa.res$weights[,2])
+}
+vals.two <- apply(tmp, c(1,2), mean)
+vals.three <- score.mat[,3,1] %*% t(fa.res$weights[,3])
+tmp <- tmp.in$outdata
+for(q in 1:dim(score.mat)[3]){
+  tmp[,,q] <- score.mat[,3,q]%*% t(fa.res$weights[,3])
+}
+vals.three <- apply(tmp, c(1,2), mean)
+tmp <- tmp.in$outdata
+for(q in 1:dim(score.mat)[3]){
+  tmp[,,q] <- score.mat[,4,q]%*% t(fa.res$weights[,4])
+}
+vals.four <- apply(tmp, c(1,2), mean)
+
+# This is essentially what figure 2 is plotting - acorss averaged across all epochs
+# Now plot these bad boys
+to.plot.one <- melt(vals.one)
+to.plot.two <- melt(vals.two)
+to.plot.three <- melt(vals.three)
+to.plot.four <- melt(vals.four)
+plot.one <- ggplot(to.plot.one, aes(x=Var1, y=Var2, fill=range01(value))) +
+  geom_tile() +
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue",  high = "red", mid = "white", 
+                       midpoint=.5, limit = c(0,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1),legend.position='none') +
+  coord_fixed()
+plot.two <- ggplot(to.plot.two, aes(x=Var1, y=Var2, fill=range01(value))) +
+  geom_tile() +
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", mid = "red", high = "white", 
+                       midpoint = .5, limit = c(range(to.plot.two$value)[1],range(to.plot.two$value)[2]), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1),legend.position='none') +
+  coord_fixed()
+plot.three <- ggplot(to.plot.three, aes(x=Var1, y=Var2, fill=range01(value))) +
+  geom_tile() +
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", mid = "red", high = "white", 
+                       midpoint = .5, limit = c(0,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1),legend.position='none') +
+  coord_fixed()
+plot.four <- ggplot(to.plot.four, aes(x=Var1, y=Var2, fill=range01(value))) +
+  geom_tile() +
+  geom_tile(color = "white")+
+  scale_fill_gradient2(low = "blue", mid = "red", high = "white", 
+                       midpoint = .5, limit = c(0,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +
+  theme_minimal()+ # minimal theme
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1),legend.position='none') +
   coord_fixed()
